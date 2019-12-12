137267f5724b591df6e767f437b7f98c73e212c3
Sam Jackson <samuelsjackson7@gmail.com>
2019-12-12 12:14:50 -0500

    connected mongoDB to app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 // initialize express
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API...' })
